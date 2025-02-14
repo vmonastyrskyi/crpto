2bc91cf10e2c105a7150f8e9e33d0080d5f0d641
vmonastyrskyi <vladyslav.monastyrskyi@gmail.com>
2025-02-14 22:13:10 +0200

    feat: add helper widgets

---
+++ lib/core/widgets/keep_alive.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class KeepAliveChild extends StatefulWidget {
+  const KeepAliveChild({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  State<KeepAliveChild> createState() => _KeepAliveChildState();
+}
+
+class _KeepAliveChildState extends State<KeepAliveChild>
+    with AutomaticKeepAliveClientMixin<KeepAliveChild> {
+  @override
+  Widget build(BuildContext context) {
+    super.build(context);
+    return widget.child;
+  }
+
+  @override
+  bool get wantKeepAlive => true;
+}
---
+++ lib/core/widgets/unfocus_tap_area.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UnfocusTapArea extends StatelessWidget {
+  const UnfocusTapArea({
+    super.key,
+    required this.child,
+  });
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      behavior: HitTestBehavior.opaque,
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: child,
+    );
+  }
+}
